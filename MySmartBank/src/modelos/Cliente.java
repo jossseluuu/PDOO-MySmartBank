a9e51fc4e3f76d545306d9bf296f29c5cb1bbdb0
jossseluuu <155114962+jossseluuu@users.noreply.github.com>
2024-10-06 22:16:05 +0200

    Iniciada la base de la clase Cliente. Closes #1 Asignados y creados los atributos de instancia y de la clase Cliente. Closes #2

---
+++ MySmartBank/src/modelos/Cliente.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+/*
+ * @author josel
+*/
+
+public class Cliente{
+    
+    // Atributos
+    private int numeroIdentificacionCliente;
+    private String documentoNacionaldeIdentidad;
+    private String nombreCompleto;
+    private enum Genero{ Masculino,Femenino};
+    private String fechaNacimiento;    
+    private int edadActual;
+    private enum nacionalidadPrincipal{España, Francia, Portugal, Italia, Otro};
+    private enum estadoCivil{Soltero, Casado, Divorciado, Viudo, Otro};
+    private String ocupacionActual;
+    private String direcciónDomicilio;
+    private int numeroTelefono;
+    private String correoElectronico;
+    private String lugarNacimiento;
+    public int numeroCuentasBancarias;
+    public int numeroTarjetas;
+    public int numeroTarjetasVirtuales;
+    public int numeroTarjetasFisicas;
+    
+    //Constructores
+    
+    //Getters y Setters
+    
+    //Otros Métodos
+    
+}
